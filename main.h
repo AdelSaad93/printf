48e076c7ffc75f05754af0990c79fed73a93b2e3
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-17 13:57:52 -0700

    printf updated code

---
+++ main.h
@@ -0,0 +1,41 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putcharac(char c);
+
+int _printcharac(va_list argums);
+
+int _printstrin(va_list argums);
+
+int _printintg(int number);
+
+int _putspecifier(const char *format, va_list argums, int *compute);
+
+int _printf(const char *format, ...);
+
+
+#endif
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdarg.h>
+#include <stdio.h>
+
+int _putcharac(char c);
+
+int _printcharac(va_list argums);
+
+int _printstrin(va_list argums);
+
+int _printintg(int number);
+
+int _putspecifier(const char *format, va_list argums, int *compute);
+
+int _printf(const char *format, ...);
+
+
+#endif
+
