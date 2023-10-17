7c8d0a450265308f1578028c0cd6d619498c974c
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-17 04:35:14 -0700

    var

--- main.c
+++ main.c
@@ -13,8 +13,11 @@ int main(void)
 	int len2;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
+
 	len2 = printf("Let's try to printf a simple sentence.\n");
+
 	_printf("length:[%d, %i]\n", len, len);
+
 	printf("Length:[%d, %i]\n", len2, len2);
 	return (0);
 }
