941d75aef0eab647ec2457e217c8ac3f08bb0370
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-17 10:35:16 -0700

    var

--- _putspecifier.c
+++ _putspecifier.c
@@ -12,7 +12,6 @@
 int _putspecifier(const char *format, va_list argums, int *compute)
 {
 	int count = 0;
-	int number;
 
 	switch (*format)
 					{
@@ -25,10 +24,6 @@ int _putspecifier(const char *format, va_list argums, int *compute)
 						case '%':
 						_putcharac('%');
 						count++;
-							break;
-						case 'd':
-						number = va_arg(argums, int);
-						count = _printintg(number);
 							break;
 						default:
 						_putcharac('%');
