7c2febb13a67e339b47750504beb09747eed1384
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-18 11:34:37 -0700

    my updated printf.c

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int _printcharac(va_list argums, int *compute);
 
 int _putstring(va_list argums, int *compute);
 
-int _putintg(va_list argums, int *compute);
+void _printintger(va_list argums, int *compute);
 
 int _printintg(int number);
 
