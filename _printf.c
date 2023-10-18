11f6211fec1e58c4c380741c97a7d72a0b20e48b
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-18 09:01:25 -0700

    my updated printf

--- _printf.c
+++ _printf.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * _printf - Function to Handle Format Specificers.
  * @format: String Containing Specifiers.
@@ -12,23 +11,94 @@ int _printf(const char *format, ...)
 
 		va_start(argums, format);
 
-			while (*format)
+			while (*format != '\0')
 			{
-				if (*format != '%')
+				if (*format == '%')
 				{
-					putchar(*format);
-					compute++;
+					format++;
+					switch (*format)
+					{
+					case 'c':
+						_printcharac(argums, &compute);
+						break;
+					case 's':
+						_putstring(argums, &compute);
+						break;
+					case '%':
+						_putpercent(&compute);
+						break;
+					default:
+						break;
+					}
 				}
 				else
 				{
-					format++;
-					compute += _putspecifier(format, argums, &compute);
+					_putcharac(*format);
+					compute++;
 				}
-
 				format++;
 			}
-
-			va_end(argums);
+		va_end(argums);
 			return (compute);
+}
+
+/**
+ * _printcharac - Print a Character.
+ *
+ * @argums: List Of Arguments.
+ * @compute: Pointer to the total character count.
+ *
+ * Return: The Number Of Characters.
+ *
+*/
+int _printcharac(va_list argums, int *compute)
+{
+		char c = va_arg(argums, int);
+
+		_putcharac(c);
+
+		(*compute)++;
+
+		return (1);
+}
+
+/**
+ * _putstring - Print a String
+ *
+ * @argums: List Of Arguments.
+ * @compute: Pointer to the total character count.
+ *
+ * Return: The Number Of Characters.
+ *
+*/
+int _putstring(va_list argums, int *compute)
+{
+		char *strin = va_arg(argums, char *);
+		int leng  = 0;
+
+		if (strin == NULL)
+			strin = "(null)";
+		while (*strin)
+		{
+			_putcharac(*strin);
+			strin++;
+			(*compute)++;
+		}
+		return (leng);
+}
+
+/**
+ * _putpercent - Print a percent character '%'.
+ * @compute: Pointer to the total character count.
+ *
+ * Return: The number of characters printed.
+ */
+
+int _putpercent(int *compute)
+{
+	_putcharac('%');
+	(*compute)++;
+
+	return (1);
 }
 
