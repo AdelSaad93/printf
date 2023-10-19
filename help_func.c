5e2c3e12061ef020061486d0deb82e1561d8dc87
AdelSaad93 <adelmohamedsaad1993@gmail.com>
2023-10-19 12:51:54 -0700

    Updated _printf.c

--- help_func.c
+++ help_func.c
@@ -52,7 +52,8 @@ int _putstring(va_list argums)
 
 int _putpercent(void)
 {
-	return (_putcharac('%'));
+	_putcharac('%');
+	return (1);
 }
 /**
  * _putcharac - Writes a Character.
@@ -87,3 +88,4 @@ int _puts(const char *str)
 
 	return (compute);
 }
+
